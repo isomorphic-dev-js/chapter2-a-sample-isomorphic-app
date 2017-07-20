de1e1323c3aa6f5aafad07e0fb6056c7fb63df82
elyseko <elyse1213+github@gmail.com>
2017-07-19 23:59:45 -0700

    feat: remove react components that will be add

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,30 +1,7 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import Recipes from './recipes';
-import Featured from './featured';
-import * as actionCreators from '../action-creators';
 
-class App extends React.Component {
-
-  render() {
-    return (
-      <div>
-        <div className="ui fixed inverted menu">
-          <div className="ui contianer">
-            <a href="/" className="header item">Recipes Example App</a>
-          </div>
-        </div>
-        <div className="ui padded grid">
-          <Recipes {...this.props}/>
-          <Featured {...this.props.featuredRecipe}/>
-        </div>
-        <div className="ui inverted vertical footer segment">
-        Footer
-        </div>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return <div></div>;
+};
 
 export default App;
