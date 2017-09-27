24521453cc5c26cb0ef857fcaa04159ba6ef3738
elyseko <elyse1213+github@gmail.com>
2017-09-26 17:42:01 -1000

    chore: add placeholder content for featured and recipes so there is something to see

---
+++ src/components/featured.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Featured = () => {
+  return <div className="ui six wide column">Featured!</div>;
+};
+
+export default Featured;
---
+++ src/components/recipes.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Recipes = () => {
+  return <div className="ui six wide column">Recipes!</div>;
+};
+
+export default Recipes;
